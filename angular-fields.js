4f1c0eddb4e103e37d1af17e2003bac042eaa32e
Willian Krause <krause.willian@gmail.com>
2016-09-09 11:35:06 -0300

    Changed some validations

--- angular-fields.js
+++ angular-fields.js
@@ -6,22 +6,23 @@
                 form: '^form'
             },
             template: `
-            <div class="form-group" ng-class="{'has-error': !$ctrl.form[$ctrl.name].$pristine && !$ctrl.form[$ctrl.name].$valid}">
-                <label class="control-label">{{$ctrl.label}}</label>
+            <div class="form-group" ng-class="{'has-error': $ctrl.form[$ctrl.name].$touched || $ctrl.form[$ctrl.name].$dirty && $ctrl.form[$ctrl.name].$invalid}">
+                <label class="control-label">{{$ctrl.label}}</label> <span class="required" ng-if="$ctrl.ngRequired">*</span>
                 <input class="form-control" type="{{$ctrl.type}}" name="{{$ctrl.name}}" ng-model="$ctrl.ngModel"
-                    ng-required="$ctrl.ngRequired" ng-readonly="$ctrl.ngReadonly" ng-disabled="$ctrl.ngDisabled" ng-maxlength="$ctrl.ngMaxlength" maxlength="{{$ctrl.ngMaxlength}}">
-                <div ng-messages="$ctrl.form[$ctrl.name].$error" class="error" role="alert" ng-if="$ctrl.form[$ctrl.name].$dirty">
-                    <div ng-message="required">O campo {{$ctrl.label}} é obrigatório.</div>
-                    <div ng-message="email">O campo {{$ctrl.label}} deve ser um e-mail válido.</div>
-                    <div ng-message="maxlength">O campo {{$ctrl.label}} deve ter no máximo {{$ctrl.ngMaxlength}} caracteres.</div>
+                    ng-required="$ctrl.ngRequired" ng-readonly="$ctrl.ngReadonly" ng-disabled="$ctrl.ngDisabled" ng-maxlength="$ctrl.ngMaxlength" maxlength="{{$ctrl.ngMaxlength}}" ng-minlength="$ctrl.ngMinlength">
+                <div ng-messages="$ctrl.form[$ctrl.name].$error" class="error" role="alert" ng-if="$ctrl.form[$ctrl.name].$touched || $ctrl.form[$ctrl.name].$dirty">
+                    <label class="control-label" ng-message="required">O campo {{$ctrl.label}} é obrigatório.</label>
+                    <label class="control-label" ng-message="email">O campo {{$ctrl.label}} deve ser um e-mail válido.</label>
+                    <label class="control-label" ng-message="maxlength">O campo {{$ctrl.label}} deve ter no máximo {{$ctrl.ngMaxlength}} caracteres.</label>
+                    <label class="control-label" ng-message="minlength">O campo {{$ctrl.label}} deve ter no mínimo {{$ctrl.ngMinlength}} caracteres.</label>
                 </div>
             </div>
             `,
             controller: function () {
                 this.type = this.type || 'text';
             },
-            link: function(scope, element) {
-                scope.watch('ngMaxlength', function(newVal) {
+            link: function (scope, element) {
+                scope.watch('ngMaxlength', function (newVal) {
                     element.find('input').attr('maxlength', newVal);
                 });
             },
@@ -33,7 +34,8 @@
                 ngRequired: '=?',
                 ngReadonly: '=?',
                 ngDisabled: '=?',
-                ngMaxlength: '=?'
+                ngMaxlength: '=?',
+                ngMinlength: '=?'
             }
         });
 })();
